2bc6317ac3ad2ce39c8463ed3b2b772734008c79
jhonathanfeijo <jhonathanfeijomoraes@gmail.com>
2023-05-07 22:17:28 -0400

    Habilitando cross origin

--- Compayz/src/main/java/com/compayz/controller/ClienteController.java
+++ Compayz/src/main/java/com/compayz/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -31,6 +32,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/cliente")
+@CrossOrigin(value = "*")
 public class ClienteController {
 
 	@Autowired
